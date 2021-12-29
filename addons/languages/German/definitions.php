2063e1be9e4604fc2ff3a2786d39fa385608a037
vanGato <49470908+phpSoftware@users.noreply.github.com>
2021-12-29 11:33:18 +0100

    Update definitions.php
    v2

--- addons/languages/German/definitions.php
+++ addons/languages/German/definitions.php
@@ -9,7 +9,7 @@ ET::$languageInfo["German"] = array(
 	"version" => ESOTALK_VERSION,
   "author" => "esoTalk Team",
   "authorEmail" => "5557720max@gmail.com",
-  "authorURL" => "https://github.com/phpSoftware/esoTalk-2020/",
+  "authorURL" => "https://github.com/phpSoftware/esoTalk-v2/",
 	"license" => "GPLv2"
 );
 
