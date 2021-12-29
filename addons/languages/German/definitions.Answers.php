6646a0c7c72940f1d44be924c0b7e982209d9442
vanGato <49470908+phpSoftware@users.noreply.github.com>
2021-12-29 11:20:02 +0100

    Update definitions.Answers.php
    Bugfix - now german works correctly for this plugin

--- addons/languages/German/definitions.Answers.php
+++ addons/languages/German/definitions.Answers.php
@@ -1,7 +1,9 @@
 <?php
 $definitions["Answer"] = "Antwort";
 $definitions["Answered"] = "Beantwortet";
-$definitions["This post answered my question"] = "Dieser Beitrag beantwortet meine Frage";
 $definitions["Answered by %s"] = "Antwort von %s";
-$definitions["See post in context"] = "Im Kontext betrachten";
+$definitions["label.answered"] = "Beantwortet";
 $definitions["Remove answer"] = "Antwort entfernen";
+$definitions["See post in context"] = "Im Kontext betrachten";
+$definitions["This answers my question"] = "Dieser Beitrag beantwortet meine Frage";
+$definitions["This answers the question"] = "Dieser Beitrag beantwortet die Frage";
