160dc801904b40507f051517062e161239db1aa8
Roman <64708726+madromas@users.noreply.github.com>
2022-04-09 09:20:35 -0400

    Add files via upload

---
+++ languages/English/definitions.Likes.php
@@ -0,0 +1,6 @@
+<?PHP
+$definitions["Unlike"] = "Unlike";
+$definitions["Like"] = "Like";
+$definitions["Members Who Liked This Post"] = "Members Who Liked This Post";
+$definitions["%s likes this."] = "%s likes this.";
+$definitions["%s like this."] = "%s like this.";
