743d5f27cbe0a87b5b2ed2ca676d907ee1ed1fd0
Roman <64708726+madromas@users.noreply.github.com>
2023-08-04 16:06:49 -0400

    Update default.master.php

--- core/views/default.master.php
+++ core/views/default.master.php
@@ -32,40 +32,15 @@ if (!defined("IN_ESOTALK")) exit;
     <link rel="shortcut icon" type="image/png" href="<?php echo URL('',true); ?>favicon.png" />
   <?php } ?> 
   <link rel="manifest" href="/manifest.json">
-  <link rel="apple-touch-icon-precomposed" sizes="180x180" href="../fav/apple-touch-icon.png" />
-  <link rel="apple-touch-icon-precomposed" sizes="57x57" href="../fav/apple-touch-icon-57x57.png" />
-<link rel="apple-touch-icon-precomposed" sizes="114x114" href="../fav/apple-touch-icon-114x114.png" />
-<link rel="apple-touch-icon-precomposed" sizes="72x72" href="../fav/apple-touch-icon-72x72.png" />
-<link rel="apple-touch-icon-precomposed" sizes="144x144" href="../fav/apple-touch-icon-144x144.png" />
-<link rel="apple-touch-icon-precomposed" sizes="60x60" href="../fav/apple-touch-icon-60x60.png" />
-<link rel="apple-touch-icon-precomposed" sizes="120x120" href="../fav/apple-touch-icon-120x120.png" />
-<link rel="apple-touch-icon-precomposed" sizes="76x76" href="../fav/apple-touch-icon-76x76.png" />
-<link rel="apple-touch-icon-precomposed" sizes="152x152" href="../fav/apple-touch-icon-152x152.png" />
-<link rel="icon" type="image/png" href="../../fav/favicon-196x196.png" sizes="196x196" />
-<link rel="icon" type="image/png" href="../fav/favicon-96x96.png" sizes="96x96" />
-<link rel="icon" type="image/png" href="../fav/favicon-32x32.png" sizes="32x32" />
-<link rel="icon" type="image/png" href="../fav/favicon-16x16.png" sizes="16x16" />
-<link rel="icon" type="image/png" href="../fav/favicon-128.png" sizes="128x128" />
-<meta name="application-name" content="&nbsp;"/>
-<meta name="msapplication-TileColor" content="#FFFFFF" />
-<meta name="msapplication-TileImage" content="../fav/mstile-144x144.png" />
-<meta name="msapplication-square70x70logo" content="../fav/mstile-70x70.png" />
-<meta name="msapplication-square150x150logo" content="../fav/mstile-150x150.png" />
-<meta name="msapplication-wide310x150logo" content="../fav/mstile-310x150.png" />
-<meta name="msapplication-square310x310logo" content="../fav/mstile-310x310.png" />
 <link rel="stylesheet" type="text/css" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css">
 <link rel="shortcut icon" href="/favicon.png" />
 <link rel="stylesheet" type="text/css" href="/fancybox/fancybox.css">
 <link type="text/css" href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,300;1,400;1,500&display=swap" rel="stylesheet">
 <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1828851643911658"
      crossorigin="anonymous"></script>	 
-
 <script src='https://cdn.jsdelivr.net/gh/Web-Highlights/webhighlights-link-preview/dist/main.js' type='module'>
 </head>
 <body class='<?php echo $data["bodyClass"]; ?>'>
-
-
-
 <script>
 //Infinite Scroll
 $(function(){ //on document ready
